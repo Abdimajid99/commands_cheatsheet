fc678a45236c5445be7f2456b4bb976a34096264
Abdimajid Shire <Abdulmjeedshire3@gmail.com>
2024-08-06 07:47:56 +0300

    some clean up work

--- install_cch.sh
+++ install_cch.sh
@@ -2,3 +2,29 @@
 set -eo pipefail
 
 echo "hello world"
+
+
+SCRIPT_NAME="cch"
+INSTALL_DIR="/usr/local/bin/commands_cheatsheet"
+SCRIPT_URL="https://commands-cheatsheet.vercel.app/commands_cheatsheet"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# create /commands_cheatsheet directory.
+mkdir $INSTALL_DIR
+
+# fetch the script and its files.
+curl -o $INSTALL_DIR/cch "$SCRIPT_URL"/cch
+curl -o $INSTALL_DIR/commands.txt "$SCRIPT_URL"/commands.txt
+curl -o $INSTALL_DIR/script_template "$SCRIPT_URL"/script_template
+
+# Make the script executable
+chmod +x $INSTALL_DIR/$SCRIPT_NAME
+
+#create a symlink in /usr/local/bin becuase it's in $PATH
+sudo ln -s $INSTALL_DIR/$SCRIPT_NAME /usr/local/bin/$SCRIPT_NAME
+
+echo "$SCRIPT_NAME installed successfully in $INSTALL_DIR"
